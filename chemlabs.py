e2712ff63b29c42f838081c7cef517b7ceb052ff
harshjadhav890 <81370729+harshjadhav890@users.noreply.github.com>
2021-05-26 13:10:32 +0530

    Update chemlabs.py

--- chemlabs.py
+++ chemlabs.py
@@ -7,8 +7,8 @@ from apps import conductometry, ph_metry, colorimetry
 st.set_page_config(layout="wide")
 
 
-st.image("Chemlabs_transparent.png", width=200)
-st.title("Welcome to Chemlabs")
+st.image("virchem-logos_transparent.png", width=200)
+st.title("Welcome to Virchem")
 st.markdown("""A simple **Data-App** for performing Chemistry experiments
 """)
 st.markdown("---")
@@ -25,7 +25,7 @@ app.add_app("Colorimetry", colorimetry.app)
 app.run()
 
 help_bar = st.sidebar.beta_expander("Help")
-help_bar.markdown("""**Chemlabs** is a data app that allows you to perform experiments involving graphical calculations
+help_bar.markdown("""**Virchem** is a data app that allows you to perform experiments involving graphical calculations
  with ease. The app accepts values for different readings depending on the 
  experiment, saves them in the form of a python dataframe and plots the graphs required. """)
 
